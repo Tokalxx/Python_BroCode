2a30de745ab198060109ac7d403ee9cf5e5b0731
SeanSM-NAB <SeanSMaseola@gmail.com>
2026-01-11 23:10:00 +0200

    Type Casting

---
+++ VARIABLES/main.py
@@ -0,0 +1,27 @@
+# String
+saber = "Artoria"
+rider = "Ushiwakamaru"
+alter_ego = "Bazett"
+
+# Integer
+four_star = 5
+three_star = 5
+five_star = 5
+
+# Float
+attack_value = 45.93
+defense_points = 45.93
+
+# Boolean 
+is_female = True
+is_male= False
+
+
+print(f"Servant Name: {saber}")
+print(f"Rarity: {four_star}")
+print(f"Atk: {attack_value}")
+
+if is_female:
+    print("Is female")
+else:
+    print("Is Male")
