35811c23db70c45e24839908641496bd7f3b07b1
SeanSM-NAB <SeanSMaseola@gmail.com>
2026-01-16 20:04:23 +0200

    input

---
+++ Input/main.py
@@ -0,0 +1,7 @@
+name = input("Enter name:")
+age = int(input("Enter age:"))
+dream = input("What do you want to do:")
+
+print(f"My name is {name}")
+print(f"I am a {age} old male")
+print(f"I want to do {dream} in the future")
